1437146890212c6543c06e53568ada090e2116e5
Frank Tran <franktranvantu@gmail.com>
2021-02-02 21:34:10 +0700

    Update

--- final/js/app.js
+++ final/js/app.js
@@ -10,16 +10,7 @@ $('.review p').html('<strong>Bun Dau Mam Tom</strong> is a famous food in Vietna
 // 4. Change the name of the app. Change *only* the text inside the span element within the h1.
 $('h1 span').text('Frank!');
 
-// 5. Create an animation for the paragraph with the id of "flashMessage." Chain an animation that 
-  $('#flashMessage')
-  // slides the message down for 1 second
-  .slideDown(1000)
-  // delays the message for 2 seconds
-  .delay(2000)
-  // slides the message up for 1 second
-  .slideUp(1000);
-
-// 6. Inside the click handler, create a working restaurant review form.
+// 5. Inside the click handler, create a working restaurant review form.
 $('#submitBtn').click(function() {
   //  a. Use the val() method to get what's been entered into the restaurant name and restaurant review form input fields. Save both values to variables.
   const restaurantName = $('#restaurantNameInput').val();
@@ -28,6 +19,15 @@ $('#submitBtn').click(function() {
   $('#newRestaurant h3').text(restaurantName);
   //  c. Insert the restaurant review value into the #newRestaurant paragraph tag
   $('#newRestaurant p').text(restaurantReview);
+
+  // 6. Create an animation for the paragraph with the id of "flashMessage." Chain an animation that
+  $('#flashMessage')
+    // slides the message down for 1 second
+    .slideDown(1000)
+    // delays the message for 2 seconds
+    .delay(2000)
+    // slides the message up for 1 second
+    .slideUp(1000);
 });
 
 
